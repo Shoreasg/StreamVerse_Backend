11abdc662cdabc6b8cd19fba8c7c6d8b9e594fcb
Wei Lun <garyganweilun@gmail.com>
2022-02-01 04:35:18 +0800

    Added get clips and highlights

--- controllers/HomePageController.js
+++ controllers/HomePageController.js
@@ -9,9 +9,35 @@ router.use(express.static("public"))
 
 
 router.get("/GetFollowedChannels", (req, res) => {
-    let data
-
     axios.get(`https://api.twitch.tv/helix/streams/followed?user_id=${req.user.TwitchId}`,
+        {
+            headers: {
+                "Client-Id": process.env.TWITCH_CLIENT_ID,
+                "Authorization": `Bearer ${req.session.token}`
+            }
+        })
+        .then((data) => {
+            res.send(data.data)
+        })
+
+})
+
+router.get("/GetUserVideos", (req, res) => {
+    axios.get(`https://api.twitch.tv/helix/videos?user_id=${req.user.TwitchId}`,
+        {
+            headers: {
+                "Client-Id": process.env.TWITCH_CLIENT_ID,
+                "Authorization": `Bearer ${req.session.token}`
+            }
+        })
+        .then((data) => {
+            res.send(data.data)
+        })
+
+})
+
+router.get("/GetUserClips", (req, res) => {
+    axios.get(`https://api.twitch.tv/helix/clips?broadcaster_id=${req.user.TwitchId}`,
         {
             headers: {
                 "Client-Id": process.env.TWITCH_CLIENT_ID,
